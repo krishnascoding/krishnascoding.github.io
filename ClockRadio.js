28c951ece57b384a449785e98ee71a9c7b8a3f8a
Krishna <krishnadrum@gmail.com>
2016-05-16 01:18:30 -0400

    Added .js

--- ClockRadio.js
+++ ClockRadio.js
@@ -1,6 +1,4 @@
-[{"stations":[{"name": "Station 1","city": "New York, NY","coordinate": "41.9471939,-87.6565108","url": "http://http-live.sr.se/p3-mp3-192","timeZone": "America/New_York",
-         "stationID": "0001",
-         "flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/US_flag_48_stars.svg/220px-US_flag_48_stars.svg.png"
+[{"stations":[{"name": "Station 1","city": "New York, NY","coordinate": "41.9471939,-87.6565108","url": "http://http-live.sr.se/p3-mp3-192","timeZone": "America/New_York","stationID": "0001","flag": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/US_flag_48_stars.svg/220px-US_flag_48_stars.svg.png"
       },
       {
          "name": "Station 2”,
